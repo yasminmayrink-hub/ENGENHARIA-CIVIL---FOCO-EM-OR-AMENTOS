f9ebb42795e0d82b843993328bad5ee0dbdb7296
yasminmayrink-hub <yasminmayrink@gmail.com>
2026-05-23 11:58:57 -0300

    corrige import do HuggingFaceEmbeddings

--- rag.py
+++ rag.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 import os
 
